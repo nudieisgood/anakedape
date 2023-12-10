02b98cd837ad083ce0bfdae8455e6a715ac8f6a7
nudieisgood <nudieisgoodmii@gmail.com>
2023-12-10 13:04:51 +0800

    scss wip

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,33 +22,33 @@ import {
   EditFeature,
   AddFeature,
   ErrorPage,
-} from "./Page";
+} from "./pages";
 
 //import actions and loaders
-import { loader as featuresLoader } from "./Page/Feature/Features";
-import { loader as featureLoader } from "./Page/Feature/Feature";
-import { loader as itemsLoader } from "./Page/Item/Items";
-import { loader as itemLoader } from "./Page/Item/Item";
-import { loader as cartLoader } from "./Page/Cart/Cart";
+import { loader as featuresLoader } from "./pages/Feature/Features";
+import { loader as featureLoader } from "./pages/Feature/Feature";
+import { loader as itemsLoader } from "./pages/Item/Items";
+import { loader as itemLoader } from "./pages/Item/Item";
+import { loader as cartLoader } from "./pages/Cart/Cart";
 import {
   loader as checkoutLoader,
   action as checkoutAction,
-} from "./Page/Checkout/Checkout";
-import { loader as homeLoader } from "./Page/Home/Home";
+} from "./pages/Checkout/Checkout";
+import { loader as homeLoader } from "./pages/Home/Home";
 import {
   loader as editItemLoader,
   action as editItemAction,
-} from "./Page/Admin/EditItem";
-import { loader as ordersLoader } from "./Page/Admin/Orders";
-import { loader as editFeatureLoader } from "./Page/Admin/AddFeature";
+} from "./pages/Admin/EditItem";
+import { loader as ordersLoader } from "./pages/Admin/Orders";
+import { loader as editFeatureLoader } from "./pages/Admin/AddFeature";
 import {
   loader as addFeatureLoader,
   action as addFeatureAction,
-} from "./Page/Admin/AddFeature";
-import { loader as adminLoader } from "./Page/Admin/AdminLayout";
-import { action as addItemAction } from "./Page/Admin/ManageItems";
-import { action as editFeatureAction } from "./Page/Admin/EditFeature";
-import { action as registerAction } from "./Page/User/Register";
+} from "./pages/Admin/AddFeature";
+import { loader as adminLoader } from "./pages/Admin/AdminLayout";
+import { action as addItemAction } from "./pages/Admin/ManageItems";
+import { action as editFeatureAction } from "./pages/Admin/EditFeature";
+import { action as registerAction } from "./pages/User/Register";
 
 const router = createBrowserRouter([
   {
